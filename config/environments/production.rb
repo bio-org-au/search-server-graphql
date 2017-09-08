eb9de74bda9e76964418e84ce9f117329ec5db54
Greg Clarke <gregbclarke@gmail.com>
2017-09-08 10:27:57 +1000

    Refer to ssg-config.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,18 @@ Rails.application.configure do
   config.action_controller.relative_url_root = "/nsl/ssg"
 end
 
+begin
+  config_file_path = "#{ENV['HOME']}/.nsl/ssg-config.rb"
+  puts "Loading config from: #{config_file_path}"
+  load config_file_path
+rescue LoadError
+  puts "=" * 100
+  puts "Unable to find the config file: #{config_file_path}"
+  puts "Application start up will now fail."
+  puts "=" * 100
+  raise
+end
+
 Rails.application.config.database_yml_file_path =
   "#{ENV['HOME']}/.nsl/ssg-database.yml"
 puts "Rails.application.config.database_yml_file_path:
