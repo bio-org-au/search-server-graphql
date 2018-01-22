cf46c11726bfe0ca88e03f18b333aaca41079fb6
Greg Clarke <gregbclarke@gmail.com>
2018-01-22 15:25:39 +1100

    Tracking down bug apparent in test only.

--- app/models/name_rank/search/history.rb
+++ app/models/name_rank/search/history.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 # Class for name_history_type
-class Name::Search::History
-  attr_reader :name_usages, :synonym_bunch
+class NameRank::Search::History
+  attr_reader :rank_name_usages, :synonym_bunch
 
   def initialize(name_id)
-    Rails.logger.debug('Name::Search::History initialize')
+    Rails.logger.debug('NameRank::Search::History initialize')
     @name = Name.find(name_id)
     raw_results = Name::Search::UsageQuery.new(name_id).results
     instance_ids = raw_results.map(&:instance_id)
     @synonym_bunch = Name::Search::Synonym::BunchQuery.new(instance_ids)
     unless @synonym_bunch.results.empty?
-      @name_usages = raw_results.collect do |usage|
+      @rank_name_usages = raw_results.collect do |usage|
         Name::Search::Usage.new(usage, @synonym_bunch)
       end
     end
--- app/models/reference/search/history.rb
+++ app/models/reference/search/history.rb
@@ -2,7 +2,7 @@
 
 # Class for name_history_type
 class Reference::Search::History
-  attr_reader :name_usages, :synonym_bunch
+  attr_reader :ref_name_usages, :synonym_bunch
 
   def initialize(name_id)
     Rails.logger.debug('Reference::Search::History initialize')
@@ -11,7 +11,7 @@ class Reference::Search::History
     instance_ids = raw_results.map(&:instance_id)
     @synonym_bunch = Reference::Search::Synonym::BunchQuery.new(instance_ids)
     unless @synonym_bunch.results.empty?
-      @name_usages = raw_results.collect do |usage|
+      @ref_name_usages = raw_results.collect do |usage|
         Reference::Search::Usage.new(usage, @synonym_bunch)
       end
     end
