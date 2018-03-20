0f3e336894df332463f964f7c571ecc0a7bbbc9f
Greg Clarke <gregbclarke@gmail.com>
2018-03-20 15:59:10 +1100

    Convert x to multiplication symbol for name fields in response.

--- app/graphql/types/name_type.rb
+++ app/graphql/types/name_type.rb
@@ -3,9 +3,24 @@
 Types::NameType = GraphQL::ObjectType.define do
   name 'name'
   field :id, types.ID
-  field :simple_name, types.String
-  field :full_name, types.String
-  field :full_name_html, types.String
+  field :simple_name do
+    type types.String
+    resolve -> (obj, args, ctx) {
+      obj.simple_name.gsub(/ x /,' × ')
+    }
+  end
+  field :full_name do
+    type types.String
+    resolve -> (obj, args, ctx) {
+      obj.full_name.gsub(/ x /,' × ')
+    }
+  end
+  field :full_name_html do
+    type types.String
+    resolve -> (obj, args, ctx) {
+      obj.full_name_html.gsub(/ x /,' × ')
+    }
+  end
   field :name_status_name, types.String
   field :family_name, types.String
   field :name_rank_name, types.String
