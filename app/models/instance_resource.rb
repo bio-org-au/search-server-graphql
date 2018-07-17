97da43b7333bf66d03f3ab119bb150aa40373f79
Greg Clarke <gregbclarke@gmail.com>
2018-07-18 09:59:15 +1000

    NSL-2347: follow-up protologue link with by referring to 'instance_resource' not 'instance_resources'

--- app/models/instance_resource.rb
+++ app/models/instance_resource.rb
@@ -2,9 +2,10 @@
 
 # Rails model
 class InstanceResource < ActiveRecord::Base
-  self.table_name = 'instance_resources'
+  self.table_name = 'instance_resource'
   self.primary_key = 'instance_id, resource_id'
 
   belongs_to :instance
   belongs_to :resource
+  
 end
