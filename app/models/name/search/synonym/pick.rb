dc13df6d3c43c531b6c5256c7cea91f6c3e2da7e
Greg Clarke <gregbclarke@gmail.com>
2018-08-28 17:06:42 +1000

    More crazy differences for boolean column between dev and test.

--- app/models/name/search/synonym/pick.rb
+++ app/models/name/search/synonym/pick.rb
@@ -24,7 +24,8 @@ class Name::Search::Synonym::Pick
     prev_type_name = ''
     @synonym_bunch.results.each do |instance|
       # true works in dev, 't' works in test!
-      if instance.misapplied == true || instance.misapplied == 't'
+      # if instance.misapplied == true || instance.misapplied == 't'
+      if instance.misapplied == 't'
         debug("misapplied")
         debug("instance.name_id: #{instance.name_id}")
         debug("instance.full_name: #{instance.name.full_name}")
