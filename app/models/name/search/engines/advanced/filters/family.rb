332bb748aae5d967200fe9bc3306667e985f66ac
Greg Clarke <gregbclarke@gmail.com>
2018-11-28 11:52:52 +1100

    NSL-3075: Make a family search return the family itself.

--- app/models/name/search/engines/advanced/filters/family.rb
+++ app/models/name/search/engines/advanced/filters/family.rb
@@ -3,20 +3,32 @@
 # Add a filter to the sql to answer a request.
 class Name::Search::Engines::Advanced::Filters::Family
   PARAMETER = 'family'
-  CLAUSE = 'name.family_id in (select id from name family where (lower(f_unaccent(family.simple_name)) like lower(f_unaccent(?))))'
+  SELECT = 'select id from name family where '
+  LFA = ' lower(f_unaccent'
+  SIMPLE = " (#{LFA}(family.simple_name)) like #{LFA}(?)))"
+  FULL = " (#{LFA}(family.full_name)) like #{LFA}(?)))"
+  IN_FAMILY = "name.family_id in ( #{SELECT} #{SIMPLE} OR #{FULL})"
+  ITSELF_SIMPLE = " (#{LFA}(name.simple_name)) like #{LFA}(?)))"
+  ITSELF_FULL = " (#{LFA}(name.simple_name)) like #{LFA}(?)))"
+  FAMILY_RANK = " name_rank.name = 'Familia' "
+  THE_NAME_ITSELF = "((#{ITSELF_SIMPLE} OR #{ITSELF_FULL}) AND #{FAMILY_RANK})"
+  CLAUSE = "(#{IN_FAMILY} OR #{THE_NAME_ITSELF})"
 
   def initialize(incoming_sql, parser)
     @incoming_sql = incoming_sql
     @parser = parser
+    @param = parameter
   end
 
   def sql
-    return @incoming_sql if parameter.blank?
-    @incoming_sql.where([CLAUSE, parameter])
+    return @incoming_sql if @param.blank?
+
+    @incoming_sql.where([CLAUSE, @param, @param, @param, @param])
   end
 
   def parameter
     return nil unless @parser.text_arg?(PARAMETER)
+
     "#{@parser.args[PARAMETER].strip.tr('*', '%')}%"
   end
 end
