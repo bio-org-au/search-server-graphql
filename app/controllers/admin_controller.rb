fe4a2d79c3425108d26395184b448685b31d6f1e
Greg Clarke <gregbclarke@gmail.com>
2018-11-28 11:51:43 +1100

    Added a home page, settings page and a change page.

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Controller
+class AdminController < ApplicationController
+  def home
+  end
+
+  def settings
+  end
+
+  def changes
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/v1', to: 'graphql#execute'
   post '/v1', to: 'graphql#execute'
+  match '/home', as: 'home', to: 'admin#home', via: :get
+  match '/settings', as: 'settings', to: 'admin#settings', via: :get
+  match '/changes', as: 'changes', to: 'admin#changes', via: :get
+  root to: 'admin#home'
 end
