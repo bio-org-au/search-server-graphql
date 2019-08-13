9173e24e464b0d1515816fe426375daefdfc9854
Greg Clarke <gregbclarke@gmail.com>
2019-08-13 16:30:46 +1000

    Adjust paginator info; generate new schema; remove demo type.

--- app/graphql/types/post_attributes_type.rb
+++
@@ -1,3 +0,0 @@
-class Types::PostAttributesType < Types::BaseInputObject
-  description "Attributes for creating or updating a blog post"
-end
--- app/models/paginator_info.rb
+++ app/models/paginator_info.rb
@@ -1,5 +1,7 @@
-class PaginatorInfo
+# frozen_string_literal: true
 
+# Generate standard pageinator info.
+class PaginatorInfo
   def initialize(per_page, page, total, offset)
     @per_page = per_page
     @page = page
@@ -12,22 +14,36 @@ class PaginatorInfo
     ostruct.count = @per_page
     ostruct.currentPage = @page
     ostruct.perPage = @per_page
-    last_page = (@total/@per_page) + 1
-    if @offset > @total
-      ostruct.firstItem = nil
-      ostruct.lastItem = nil
-    else
-      ostruct.firstItem = (@per_page * (@page - 1)) + 1
-      if @page < last_page
-        ostruct.lastItem = (@per_page * @page)
-      else
-        ostruct.lastItem = @total
-      end
-    end
-    ostruct.lastPage = (@total/@per_page) + 1
+    build_last_page
+    build_first_and_last_item
+    ostruct.firstItem = @first_item
+    ostruct.lastItem = @last_item
+    ostruct.lastPage = @last_page
     ostruct.hasMorePages = @total > (@per_page * @page)
     ostruct.total = @total
     ostruct
   end
 
+  def build_first_and_last_item
+    if @offset >= @total
+      @first_item = @last_item = nil
+    else
+      @first_item = (@per_page * (@page - 1)) + 1
+      @last_item = if @page < @last_page
+                     (@per_page * @page)
+                   else
+                     @total
+                   end
+    end
+  end
+
+  def build_last_page
+    last_page_real = ((@total * 1.0) / @per_page)
+    last_page_int = (@total / @per_page)
+    @last_page = if last_page_real > last_page_int
+                   last_page_int + 1
+                 else
+                   last_page_int
+                 end
+  end
 end
